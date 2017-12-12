3efe1adbac11fe42370ea955c62dc0bbc3e16246
Helena <hgarcia@es.amnesty.org>
2017-12-12 10:55:52 +0100

    añadida comprobacion de regex a cuenta ESBAN

--- includes/es-ban.php
+++ includes/es-ban.php
@@ -3,65 +3,71 @@
     /***********************
     *** FROM: http://www.neleste.com/validar-ccc-con-php/
     ************************/
-    function ccc_valido($ccc)
+    function ccc_valido($entity, $office, $account)
     {
-        //$ccc sería el 20770338793100254321
-        $valido = true;
+        $valido = false;
+        $ccc = $entity."".$office."".$account;
 
-        ///////////////////////////////////////////////////
-        //    Dígito de control de la entidad y sucursal:
-        //Se multiplica cada dígito por su factor de peso
-        ///////////////////////////////////////////////////
-        $suma = 0;
-        $suma += $ccc[0] * 4;
-        $suma += $ccc[1] * 8;
-        $suma += $ccc[2] * 5;
-        $suma += $ccc[3] * 10;
-        $suma += $ccc[4] * 9;
-        $suma += $ccc[5] * 7;
-        $suma += $ccc[6] * 3;
-        $suma += $ccc[7] * 6;
+        if (respectsAccountPattern($entity, $office, $account)){
 
-        $division = floor($suma/11);
-        $resto    = $suma - ($division  * 11);
-        $primer_digito_control = 11 - $resto;
-        if($primer_digito_control == 11)
-            $primer_digito_control = 0;
+          //$ccc sería el 20770338793100254321
+          $valido = true;
 
-        if($primer_digito_control == 10)
-            $primer_digito_control = 1;
+          ///////////////////////////////////////////////////
+          //    Dígito de control de la entidad y sucursal:
+          //Se multiplica cada dígito por su factor de peso
+          ///////////////////////////////////////////////////
+          $suma = 0;
+          $suma += $ccc[0] * 4;
+          $suma += $ccc[1] * 8;
+          $suma += $ccc[2] * 5;
+          $suma += $ccc[3] * 10;
+          $suma += $ccc[4] * 9;
+          $suma += $ccc[5] * 7;
+          $suma += $ccc[6] * 3;
+          $suma += $ccc[7] * 6;
 
-        if($primer_digito_control != $ccc[8])
-            $valido = false;
+          $division = floor($suma/11);
+          $resto    = $suma - ($division  * 11);
+          $primer_digito_control = 11 - $resto;
+          if($primer_digito_control == 11)
+              $primer_digito_control = 0;
 
-        ///////////////////////////////////////////////////
-        //            Dígito de control de la cuenta:
-        ///////////////////////////////////////////////////
-        $suma = 0;
-        $suma += $ccc[10] * 1;
-        $suma += $ccc[11] * 2;
-        $suma += $ccc[12] * 4;
-        $suma += $ccc[13] * 8;
-        $suma += $ccc[14] * 5;
-        $suma += $ccc[15] * 10;
-        $suma += $ccc[16] * 9;
-        $suma += $ccc[17] * 7;
-        $suma += $ccc[18] * 3;
-        $suma += $ccc[19] * 6;
+          if($primer_digito_control == 10)
+              $primer_digito_control = 1;
 
-        $division = floor($suma/11);
-        $resto = $suma-($division  * 11);
-        $segundo_digito_control = 11- $resto;
+          if($primer_digito_control != $ccc[8])
+              $valido = false;
 
-        if($segundo_digito_control == 11)
-            $segundo_digito_control = 0;
-        if($segundo_digito_control == 10)
-            $segundo_digito_control = 1;
+          ///////////////////////////////////////////////////
+          //            Dígito de control de la cuenta:
+          ///////////////////////////////////////////////////
+          $suma = 0;
+          $suma += $ccc[10] * 1;
+          $suma += $ccc[11] * 2;
+          $suma += $ccc[12] * 4;
+          $suma += $ccc[13] * 8;
+          $suma += $ccc[14] * 5;
+          $suma += $ccc[15] * 10;
+          $suma += $ccc[16] * 9;
+          $suma += $ccc[17] * 7;
+          $suma += $ccc[18] * 3;
+          $suma += $ccc[19] * 6;
 
-        if($segundo_digito_control != $ccc[9])
-            $valido = false;
+          $division = floor($suma/11);
+          $resto = $suma-($division  * 11);
+          $segundo_digito_control = 11- $resto;
 
-        return $valido;
+          if($segundo_digito_control == 11)
+              $segundo_digito_control = 0;
+          if($segundo_digito_control == 10)
+              $segundo_digito_control = 1;
+
+          if($segundo_digito_control != $ccc[9])
+              $valido = false;
+
+          return $valido;
+        }
     }
 
    /*
